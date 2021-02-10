d6cb0112cd21af85e9491e5c9554aa6d136eb128
Mona Lisa <clarkefraser1@gmail.com>
2021-02-10 16:02:28 +1100

    Adding skeleton for index.js and class employee

--- index.js
+++ index.js
@@ -2,3 +2,29 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+
+const Employee = require("./lib/Employee");
+
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+//Input prompts
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Welcome, press enter at any time to skip. What is your name?:',
+      },
+    
+    ]);
+};  
+      
+      const init = () => {
+        promptUser()
+          .then((answers) => writeFileAsync('./sampleReadme/readme.md', generateMarkdown(answers)))
+          .then(() => console.log('Successfully wrote '))
+          .catch((err) => console.error(err));
+      };
+    
+    init();
