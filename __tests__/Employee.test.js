ec40c1fad939f35edd0c72c72617bc11e80f1bd3
Mona Lisa <clarkefraser1@gmail.com>
2021-02-11 18:05:23 +1100

    adding to employee class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,10 @@
+describe("Employee", () => {
+    it ("", () => {
+        // create a new Employee object 
+        // assert that the employees getName function returns the name I created the employee with. 
+    })
+    // It returns the name of the employee
+
+    //
+
+})
