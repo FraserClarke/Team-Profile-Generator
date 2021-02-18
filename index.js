8d806ff492bc823812fd25da882c1140a421d298
Mona Lisa <clarkefraser1@gmail.com>
2021-02-18 18:10:57 +1100

    fixing menu

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 //const generateMarkdown = require('./utils/generateMarkdown');
-const inquirer = require('inquirer');
-const fs = require('fs');
-const util = require('util');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 //import all libs
 const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const teamHTML = require("./src/template")
+const teamHTML = require("./src/template");
 
 let Team = [];
-
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -18,144 +17,166 @@ const writeFileAsync = util.promisify(fs.writeFile);
 const promptManager = () => {
   return inquirer.prompt([
     {
-        type: 'input',
-        name: 'mangaerName',
-        message: 'Welcome, press enter at any time to skip. Enter Team managers name:',
-      },
-      {
-        type: 'input',
-        name: 'employeeId',
-        message: 'Enter Managers employee ID:',
-      },
-      {
-        type: 'input',
-        name: 'emailAdress',
-        message: 'Enter Email address:',
-      },
-      {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'Enter office number:',
-      },
-      
-      
-    
-    ]);
-};  
+      type: "input",
+      name: "managerName",
+      message:
+        "Welcome, press enter at any time to skip. Enter Team managers name:",
+    },
+    {
+      type: "input",
+      name: "employeeId",
+      message: "Enter Managers employee ID:",
+    },
+    {
+      type: "input",
+      name: "emailAdress",
+      message: "Enter Email address:",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "Enter office number:",
+    },
+  ]);
+};
 
 function optionList() {
-  inquirer.prompt([{
-      type: 'list',
-      name: 'Options',
-      message: 'Add engineer, intern or finish building team:',
-      choices: ['Engineer', 'Intern', 'Finish'],
-    }])
-  .then((teamChoices) => {
-    console.log(teamChoices)
-    if(teamChoices.Options === "Engineer"){
-      addEng()
-    if(teamChoices.Options === "Intern"){
-      addIntern()
-    }
-    else(generateTeam())
-    //default(generateTeam)
-    }
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "Options",
+        message: "Add engineer, intern or finish building team:",
+        choices: ["Engineer", "Intern", "Finish"],
+      },
+    ])
+    .then((teamChoices) => {
+      console.log(teamChoices);
+      if (teamChoices.Options === "Engineer") {
+        addEng();
+      } else if (teamChoices.Options === "Intern") {
+        addIntern();
+      } else {
+        generateTeam();
+      }
+      //default(generateTeam)
 
-    // switch (new Date().getDay()) {
-    //   case 6:
-    //     text = "Today is Saturday";
-    //     break;
-    //   case 0:
-    //     text = "Today is Sunday";
-    //     break;
-    //   default:
-    //     text = "Looking forward to the Weekend";
-    // }
+      // switch (new Date().getDay()) {
+      //   case 6:
+      //     text = "Today is Saturday";
+      //     break;
+      //   case 0:
+      //     text = "Today is Sunday";
+      //     break;
+      //   default:
+      //     text = "Looking forward to the Weekend";
+      // }
 
-    //if default call the function that takes care of generating the template and writing the file
-    generateTeam();
-    // if or switch condition  based teamChoices
-  })
+      //if default call the function that takes care of generating the template and writing the file
+      generateTeam();
+      // if or switch condition  based teamChoices
+    });
 }
 
-function addEng(){
-  inquirer.prompt([
-    {
-    type: 'input',
-    name: 'name',
-    message: 'Enter engineers name:',
-    },
-    {
-      type: 'input',
-      name: 'engineerId',
-      message: 'Enter engineers ID:',
+function addEng() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter engineers name:",
       },
       {
-        type: 'input',
-        name: 'email',
-        message: 'Add engineers email adress:',
-        },
-        {
-          type: 'input',
-          name: 'github',
-          message: 'Add engineers Github Username:',
-          },
-          
-  
-  ])
-  //console.log("addEng")
-  .then(optionList);
-  //.then(optionList());
+        type: "input",
+        name: "engineerId",
+        message: "Enter engineers ID:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Add engineers email adress:",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Add engineers Github Username:",
+      },
+    ])
+    //console.log("addEng")
+    //.then(optionList);
+    //.then(optionList());
+    .then((answers) => {
+      const manager = new Manager(
+        answers.managerName,  ///CHANGE TO ENIGINEER DATA SAME AS MANGE BELOW EG TEAM.PUSH etc
+        answers.employeeId,
+        answers.emailAdress,
+        answers.officeNumber
+      );
+      // Store new engineer in team array here
+      // Call on optionList() again. 
+    });
 }
 
-
-function addIntern(){
-  inquirer.prompt([
-    {
-    type: 'input',
-    name: 'internName',
-    message: 'Add interns name:',
-    },
-    {
-      type: 'input',
-      name: 'internId',
-      message: 'Add interns ID:',
+function addIntern() {
+  console.log("addIntern");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "internName",
+        message: "Add interns name:",
       },
       {
-        type: 'input',
-        name: 'email',
-        message: 'Add interns email:',
-        },
-        {
-          type: 'input',
-          name: 'school',
-          message: 'Add interns school:',
-          },
-  
-  
-  ])
-  //console.log("addIntern")
-  .then(optionList);
+        type: "input",
+        name: "internId",
+        message: "Add interns ID:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Add interns email:",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Add interns school:",
+      },
+    ])
+
+    .then(optionList);
 }
 
 const init = () => {
-  promptManager()
-  .then((answers)=> {
+  promptManager().then((answers) => {
+    const manager = new Manager(
+      answers.managerName,
+      answers.employeeId,
+      answers.emailAdress,
+      answers.officeNumber
+    );
+
+    // console.log("Manager");
+    //console.log(answers);
     //generate Manager
-        //let manager = new manager
+    //  let manager = new manager
     //push manager to the array
-  //Team.push
+    Team.push(manager);
 
-
-  optionList()
-
-
-  })
-}
+    optionList();
+  });
+};
 
 const generateTeam = () => {
   //pass array to the template
-  teamHTML.gernerateMangerHTML
+
+  // TEAM is a global array, at this point we know what team members we have .
+
+  // initilialise a string which is the HTML string. 
+  // Iterate through each team meber in TEAM:
+  //   Get their role bycalling their getRole method.
+  //   Depending on their role, call the appropriate generateHtml function, appendto HTML string . 
+
+  //teamHTML.gernerateMangerHTML;
   //write the string to a html file using fs.writeFileSync
   //egin ///create variables
   //inter
@@ -164,7 +185,7 @@ const generateTeam = () => {
   //writefile
   //teamhtml.generateallhtml(answers))) //answers are the variables above
   //fs.writeFileSync('./template.js')
-}
+};
 
 // const init = () => {
 //   promptUser()
@@ -173,9 +194,6 @@ const generateTeam = () => {
 //     .catch((err) => console.error(err));
 // };
 
-
-
-      
 // const init = () => {
 //   promptUser()
 //     .then((answers) => {
@@ -185,10 +203,5 @@ const generateTeam = () => {
 //     .then(() => console.log('Successfully wrote '))
 //     .catch((err) => console.error(err));
 // };
-    
+
 init();
-
-
-
-
-
